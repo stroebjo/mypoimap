8c1261afdd0d54abd80e7d34ccfa318d9d492011
Jonathan Ströbele <mail@jonathanstroebele.de>
2024-08-18 13:13:09 +0200

    refactore: load EasyMDE via dynamic import in vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -31,3 +31,21 @@ window.PhotoSwipe = PhotoSwipe;
 //require('./photoswipe');
 
 
+// wrapper to dynamically load EasyMDE (~300kb js file!)
+async function loadEasyMDE(config) {
+    const easymdeModule = await import('easymde');
+    const EasyMDE = easymdeModule.default;
+
+    var editor = new EasyMDE({
+        element: document.getElementById(config.id),
+        shortcuts: {
+            drawImage: null // don't overload DevTools shortcuts if EasyMDE has focus...
+        },
+        autoDownloadFontAwesome: false,
+        promptURLs: true,
+        status: false,
+        spellChecker: false
+    });
+}
+window.loadEasyMDE = loadEasyMDE;
+
--- resources/views/javascript/easymde.blade.php
+++ resources/views/javascript/easymde.blade.php
@@ -1,15 +1,5 @@
-<script src="{{ asset('js/easymde.min.js') }}" ></script>
-
-<script>
-var easyMDE = new EasyMDE({
-    element: document.getElementById('{{ $id }}'),
-    shortcuts: {
-        drawImage: null // don't overload DevTools shorctus if EasyMDE has focus...
-    },
-    autoDownloadFontAwesome: false,
-    promptURLs: true,
-    status: false,
-    spellChecker: false
+<script type="module">
+await loadEasyMDE({
+    'id': '{{ $id }}'
 });
-
 </script>
